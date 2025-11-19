00db1459327afa9035eb38d1bee792d8bb790a1b
nlofgn <nicolaslofgren2028@u.northwestern.edu>
2025-11-19 11:22:34 -0600

    implemented player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,32 @@
 public class Player {
+    private String name;
+    private int tricksWon;
+    private boolean calledTrump;
+
+    public Player(String name) {
+        this.name = name;
+        this.tricksWon = 0;
+        this.calledTrump = false;
+    }
+
+    public void addTrick() {
+        this.tricksWon++;
+    }
+
+    public void resetTricks() {
+        this.tricksWon = 0;
+    }
+
+    public void setCalledTrump(boolean calledTrump) {
+        this.calledTrump = calledTrump;
+    }
+
+    public int getTricksWon() {
+        return tricksWon;
+    }
+
+    public boolean getCalledTrump() {
+        return calledTrump;
+    }
+
 }
